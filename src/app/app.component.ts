27d3da803a3d9fff21b177c4f31977c645e4630c
jrodva <jr.rodriguez@crowdfarming.com>
2023-08-08 19:07:29 +0200

    feat: add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { TasksService } from './services/tasks.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todo-list';
+
+  constructor(private tasksService: TasksService) {
+  }
+
+  ngOnInit() {
+    this.tasksService.getTasks().subscribe((tasks) => {
+      console.log('Tareasss ', tasks);
+    })
+  }
 }
