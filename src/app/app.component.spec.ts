bb13104e89b6ae8488320e722bd40f8813cc9892
jrodva <jr.rodriguez@crowdfarming.com>
2023-08-08 16:36:41 +0200

    add jest configuration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todo-list'`, () => {
+  it(`should have as title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('todo-list');
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('todo-list app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Base Page');
   });
 });
