00537b6d51e20f947bd3da019609a4f0ab503e80
jrodva <jr.rodriguez@crowdfarming.com>
2023-08-08 19:19:21 +0200

    chore: remove a little test to check the api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,13 @@
-import { Component, OnInit} from '@angular/core';
-import { TasksService } from './services/tasks.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'todo-list';
 
-  constructor(private tasksService: TasksService) {
-  }
+  constructor() { }
 
-  ngOnInit() {
-    this.tasksService.getTasks().subscribe((tasks) => {
-      console.log('Tareasss ', tasks);
-    })
-  }
 }
