27cca190d116add8f72145c93142d72266664f5f
jrodva <jr.rodriguez@crowdfarming.com>
2023-08-09 04:04:19 +0200

    feat: add styles and layout for buttons to edit and delete tasks

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Task } from '../interfaces/task';
+import { Status } from '../enums/status';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -18,7 +19,7 @@ export class TasksService {
       .pipe(
         map(tasks => {
           const filteredTasks = tasks.filter(task => task.status === type);
-          const orderByStatus = ["pending", "deleted", "completed"];
+          const orderByStatus = [Status.Pending, Status.Deleted, Status.Completed];
           const sortedTaskByStatus = tasks.sort(
             (a, b) => orderByStatus.indexOf(a.status) - orderByStatus.indexOf(b.status)
           );
