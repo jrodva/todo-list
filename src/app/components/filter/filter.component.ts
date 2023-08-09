ecb58ac531bc236d353cd7563631a2ba22fd734c
jrodva <jr.rodriguez@crowdfarming.com>
2023-08-10 01:41:30 +0200

    feat: add subscription to destroy when the component ended his life cycle

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,30 +1,38 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { TasksService } from '../../services/tasks.service';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent implements OnInit, OnDestroy {
   @Input() name: string;
   @Input() path: string;
 
   counter: number;
+  counterSubscription: Subscription;
 
   constructor(private tasksService: TasksService, private router: Router) { }
 
   ngOnInit() {
-    this.tasksService.tasks$.subscribe((tasks) => {
-      const filteredCounter = tasks.filter(task => task.status === this.path.substring(1)).length;
+    this.counterSubscription = this.tasksService
+      .tasks$
+      .subscribe((tasks) => {
+        const filteredCounter = tasks.filter(task => task.status === this.path.substring(1)).length;
 
-      this.counter = filteredCounter || tasks.length;
+        this.counter = filteredCounter || tasks.length;
     });
   }
 
   async filterTasksByStatus() {
     await this.router.navigate([this.path]);
     this.tasksService.updateTasksWithDataFromApi();
+  }
+
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
   }
 }
