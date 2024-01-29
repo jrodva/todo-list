d7351cb41bfb2126fe2394b1e0acb6f4f2350046
jrodva <jr.rodriguez@crowdfarming.com>
2024-01-29 12:33:38 +0100

    feat: add unit tests to check the rendered elements in the filter component

--- src/app/components/filter/filter.component.spec.ts
+++ src/app/components/filter/filter.component.spec.ts
@@ -2,16 +2,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FilterComponent } from './filter.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { expect } from '@jest/globals';
+import { Router } from '@angular/router';
 
 describe('FilterComponent', () => {
   let component: FilterComponent;
   let fixture: ComponentFixture<FilterComponent>;
+  let routerMock: jest.Mocked<Router>;
 
   beforeEach(() => {
+    routerMock = {
+      navigate: jest.fn(),
+    } as unknown as jest.Mocked<Router>;
+
     TestBed.configureTestingModule({
       declarations: [FilterComponent],
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: Router, useValue: routerMock },
+      ]
     });
+
     fixture = TestBed.createComponent(FilterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +29,24 @@ describe('FilterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update counter on task changes', () => {
+    component.name = 'Completed';
+    component.path = '/completed';
+    component.counter = 1;
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button');
+
+    expect(button.textContent).toContain('Completed (1)');
+  });
+
+  it('should navigate to the specified path on button click', async () => {
+    component.path = '/completed';
+
+    await component.filterTasksByStatus();
+
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/completed']);
   });
 });
